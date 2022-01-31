c3d2c4cd9edad71eb8abd9f0fc06db2f3b32cb62
GigaKrish <97963201+GigaKrish@users.noreply.github.com>
2022-01-31 21:46:35 +0530

    Update Day 1 take multi val adn sum.py

--- Day 1 take multi val adn sum.py
+++ Day 1 take multi val adn sum.py
@@ -1,2 +1,6 @@
-full = [int(i) for i in input('Enter numeric values: ').split()]
-print (f"The sum of given values is : {sum(full)}")
+#Day 1 (jan 31) Assignment:Take different input values and print thier sum
+
+full = [int(i) for i in input('Enter numeric values: ').split()]    #list comprehension to take feed values 
+print (f"The sum of given values is : {sum(full)}")   #f string to display data 
+#Assignment by Krish Sharma 
+#email: ksysknp@gmail.com
