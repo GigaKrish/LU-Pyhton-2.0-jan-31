f34ed98f1f8348eef30b97a1cf93215bf0c6185f
GigaKrish <97963201+GigaKrish@users.noreply.github.com>
2022-02-09 22:26:11 +0530

    Update Day 6 mod division.py

--- Day 6 mod division.py
+++ Day 6 mod division.py
@@ -1,6 +1,5 @@
 #Day 7 Assignment by Krish Sharma, email: ksysknp@gmail.com
 
-
 def mod_div(fun):
 	def deno(a, b):
 		if a < b :
@@ -11,6 +10,7 @@ def mod_div(fun):
 @mod_div
 def div(a, b):
 	return  a // b
-    
-a, b = (int(i) for i in input("Enter any two Numbers: ").split())
-print ("the answer is:", div(a, b))
+
+a, b = (int(i) for i in input("Enter any two numbers: ").split())
+
+print ("The answer is:", div(a, b))
